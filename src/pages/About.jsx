533867b9cc69f72e4687326543de8cf6052a4aa7
Dharmesh Singh <63810275+DharmeshSingh01@users.noreply.github.com>
2022-09-20 18:05:07 +0530

    Change Link

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,13 +7,13 @@ export default function About() {
       <p className='mb-4 text-2xl font-light'>
         A React app to search GitHub profiles and see profile details. This
         project is part of the
-        <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
-          {' '}
+        <a href='#'>
+         
           React GitHub Finder
-        </a>{' '}
+        </a>
         
         <strong>
-          <a href='#'> Dharmesh Singh</a>
+          <a href='https://www.c-sharpcorner.com/members/dharmesh-singh3'> Dharmesh Singh</a>
         </strong>
         .
       </p>
@@ -22,7 +22,7 @@ export default function About() {
       </p>
       <p className='text-lg text-gray-400'>
         Layout By:
-        <a className='text-white' href='https://twitter.com/hassibmoddasser'>
+        <a className='text-white' href='https://www.c-sharpcorner.com/members/dharmesh-singh3'>
           Dharmesh Singh
         </a>
       </p>
